b42d604656814f3acb39e1b7e40f432d35f7e6b9
kesh1018 <dhurkesh_requiem@live.com>
2016-04-04 15:09:06 +0800

    - Deadlock Update

--- RoundRobin.java
+++ RoundRobin.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class RoundRobin {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 		int n, sum, timeQuantum;
 		int waitingTime[], burstTimeInput[], turnaroundTime[], burstTimeOutput[];
@@ -11,8 +12,8 @@ public class RoundRobin {
 		System.out.println("===== Round Robin =====\n"); 
 		
 		Scanner num = new Scanner(System.in);
-		System.out.println("Enter number of process:"); 
-		n = num.nextInt();     			//input number of process
+		System.out.println("Enter no of process"); 
+		n = num.nextInt(); //input number of process
 		
 		waitingTime = new int[n];
 		burstTimeInput = new int[n];
@@ -21,39 +22,39 @@ public class RoundRobin {
 		
 		for(int i = 0; i < n; i++){
 			System.out.println("Enter the burst time for process " +(i+1));
-			burstTimeInput[i] = num.nextInt();  //input burst time for each process
+			burstTimeInput[i] = num.nextInt(); //input burst time for each process
 		}
 		
-		System.out.println("Enter time quantum?");
+		System.out.println("Enter time quantum?"); //input quantum time
 		
-		timeQuantum = num.nextInt();		//input quantum time
+		timeQuantum = num.nextInt();
 		
-		long cputimeBefore = System.currentTimeMillis();  //Initialize the start time of CPU usage
+		long cputimeBefore = System.currentTimeMillis(); //Initialize the start time of CPU usage
 		
 		for(int i = 0; i < n; i++){
-			burstTimeOutput[i] = burstTimeInput[i];  //copy burstTimeInput into burstTimeOutput for print
+			burstTimeOutput[i] = burstTimeInput[i]; //copy burstTimeInput into burstTimeOutput for print
 		}
 		
 		for(int i = 0; i < n; i++){
-			waitingTime[i] = 0;			//initialize elements in waiting time to zero
+			waitingTime[i] = 0; //initialize elements in waiting time to zero
 		}
 		
 		/*  ======= Round Robin Algorithm ====== */
 		
 		do {
-			for(int i = 0; i < n; i++){				
-				if(burstTimeInput[i] > timeQuantum){		//compare burstTime with quantum
-					burstTimeInput[i] -= timeQuantum;		//minus current burst time with timeQuantum
+			for(int i = 0; i < n; i++){
+				if(burstTimeInput[i] > timeQuantum){ //compare burstTime with quantum
+					burstTimeInput[i] -= timeQuantum; //minus current burst time with timeQuantum
 					
 					for(int j = 0; j < n; j++){
-						if((j != i) && (burstTimeInput[j] != 0 )){		
-							waitingTime[j] += timeQuantum;			//allocate the waiting time for the correct process
+						if((j != i) && (burstTimeInput[j] != 0 )){
+							waitingTime[j] += timeQuantum; //allocate the waiting time for the correct process
 						}
 					}
 				}else{
 					for(int j = 0; j < n; j++){
-						if((j != i) && (burstTimeInput[j] != 0)){		
-							waitingTime[j] += burstTimeInput[i];
+						if((j != i) && (burstTimeInput[j] != 0)){
+							waitingTime[j] += burstTimeInput[i]; 
 						}
 					
 					}
@@ -72,7 +73,7 @@ public class RoundRobin {
 		/*  ======= End ====== */
 		
 		for(int i = 0; i < n; i++){
-			turnaroundTime[i] = waitingTime[i] + burstTimeOutput[i];		//allocate turn around time for each process
+			turnaroundTime[i] = waitingTime[i] + burstTimeOutput[i]; //allocate turn around time for each process
 		}
 			
 		for(int j = 0; j < n; j++){
@@ -89,7 +90,7 @@ public class RoundRobin {
 		System.out.println("| Process | BurstTime | WaitingTime | TurnAroundTime |");	
 		
 		for(int i = 0; i < n; i++){
-			System.out.println("      "+ (i+1) +" \t"+burstTimeOutput[i]+"\t     "+waitingTime[i]+"\t\t    "+turnaroundTime[i]);
+			System.out.println("      "+ i +" \t"+burstTimeOutput[i]+"\t     "+waitingTime[i]+"\t\t    "+turnaroundTime[i]);
 		}
 		
 		System.out.println("\n======================================================");
@@ -98,9 +99,9 @@ public class RoundRobin {
 		
 		System.out.println("Average Turn Around Time "+ String.format("%.2f", (averageTATime = averageTATime/n)));
 		
-		long cputimeAfter = System.currentTimeMillis();		//Get the end time of CPU usage during the program
+		long cputimeAfter = System.currentTimeMillis(); //Get the end time of CPU usage during the program
 		
-		long cputimeDifference = cputimeAfter - cputimeBefore;  //Calculate CPU usage
+		long cputimeDifference = cputimeAfter - cputimeBefore; //Calculate CPU usage
 		
 		System.out.println("CPU Time After " + cputimeDifference);
 		
---
+++ deadlock/Deadlock.java
@@ -0,0 +1,116 @@
+package deadlock;
+
+import java.util.Scanner;
+
+public class Deadlock {
+	 
+	private static int p, r, needMatrix[][], allocateMatrix[][], maxMatrix[][], availMatrix[][];
+
+	public static void main(String[] args) {
+		boolean check = false;
+		do{
+			checkDeadlock(check);
+			
+		}while(check == true);
+	}
+	      
+    private static void input(){
+    	 Scanner input = new Scanner(System.in);
+     
+	     System.out.print("Enter no. of processes and resources : ");
+	     
+	     p = input.nextInt();  //no. of process
+	     r = input.nextInt(); //no. of resources
+	     
+	     needMatrix = new int[p][r];  //initializing arrays
+	     maxMatrix = new int[p][r];
+	     allocateMatrix = new int[p][r];
+	     availMatrix = new int[1][r];
+	      
+	     System.out.println("Enter allocation matrix : ");
+	     
+	     for(int i = 0; i < p; i++){
+	    	 for(int j = 0; j < r; j++){
+	    		 allocateMatrix[i][j] = input.nextInt();  //allocation matrix
+	  	       
+	          }
+	     }
+	          
+	     System.out.println("Enter max matrix : ");
+	     
+	     for(int i = 0;i < p; i++){
+	    	 for(int j = 0; j < r; j++){
+	    		 maxMatrix[i][j] = input.nextInt();  //max matrix
+	    	 }
+	    	 
+	     }
+	
+	     System.out.println("Enter available matrix : ");
+	     
+	     for(int j = 0; j < r; j++){
+	    	 availMatrix[0][j] = input.nextInt();  //available matrix
+	     }
+    }
+
+    public static boolean checkDeadlock(boolean check){
+    	
+ 	   input();
+       calculateNeedMatrix();
+       
+       boolean done[] = new boolean[p];
+       int j=0;
+      
+ 
+       while(j < p){  //until all process allocated
+    	   
+    	   boolean allocated = false;
+	       
+	       for(int i = 0;i < p; i++){
+	    	   if(!done[i] && check(i)){
+	    		   //trying to allocate
+	    		   
+	    		   for(int k = 0;k < r; k++){
+	    			   availMatrix[0][k] = availMatrix[0][k] - needMatrix[i][k] + maxMatrix[i][k];
+	    		   }
+	    			   
+	    		   System.out.println("Allocated process : "+i);
+	    		   allocated = done[i]=true;
+	               j++;
+	    	   }    
+	          if(!allocated) break;  //if no allocation
+	       }
+	       
+	       if(j == p){
+	    	   //if all processes are allocated
+	    	   System.out.println("\nSafely allocated");
+	    	   check = true;
+	       }
+	       else{
+	    	   System.out.println("Deadlock Occured");
+	    	   check = false;
+	       }
+       }
+	return check;
+    }
+    
+    private static int[][] calculateNeedMatrix(){
+    	
+       for(int i = 0; i < p; i++){
+    	   for(int j = 0; j < r; j++){ //calculating need matrix
+          	 needMatrix[i][j] = maxMatrix[i][j] - allocateMatrix[i][j];
+           }
+       }
+         
+       return needMatrix;
+    }
+  
+    private static boolean check(int i){
+       //check all process can be allocated
+       for(int j = 0; j < r; j++) 
+    	   if(availMatrix[0][j] < needMatrix[i][j])
+    		   return false;
+       
+       return true;
+    }
+
+}
